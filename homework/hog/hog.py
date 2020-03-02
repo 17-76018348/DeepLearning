b7215e42dd2c11b8a11f3454d7e7c91168781ac4
17-76018348 <53898123+17-76018348@users.noreply.github.com>
2020-03-02 15:11:59 +0900

    Update hog.py

--- homework/hog/hog.py
+++ homework/hog/hog.py
@@ -26,21 +26,22 @@ class Gradient():
         )
         self.grad_y = np.zeros_like(self.grad_x)
 
+        
+
     def set_grad(self):
-        print(self.in_y - self.fil_size + 1)
-        print(self.in_x - self.fil_size + 1)
-        for h in range(0, self.in_y - self.fil_size + 1, self.stride):
-            for w in range(0, self.in_x - self.fil_size + 1, self.stride):
-                print(h,w)
-                self.grad_x[h][w] = np.sum(self.input[h:h+3,w:w+3] * self.filter_x)
-                self.grad_y[h][w] = np.sum(self.input[h:h+3,w:w+3] * self.filter_y) 
+
+        for idx_h,h in enumerate(list(range(0, self.in_y - self.fil_size + 1, self.stride))):
+            for idx_w,w in enumerate((range(0, self.in_x - self.fil_size + 1, self.stride))):
+
+                self.grad_x[idx_h][idx_w] = np.sum(self.input[h:h+3,w:w+3] * self.filter_x)
+                self.grad_y[idx_h][idx_w] = np.sum(self.input[h:h+3,w:w+3] * self.filter_y) 
                 
     def set_grad_mag(self):
         grad_mag = np.power((np.power(self.grad_x,2) + np.power(self.grad_y,2)),1/2)
         return grad_mag
         
     def set_grad_ang(self):
-        grad_ang = np.abs(np.arctan2(self.grad_y,self.grad_x+0.00000001))
+        grad_ang = np.abs(np.arctan2(self.grad_y,self.grad_x+0.00000001))/np.pi*180
         return grad_ang
     def auto(self):
         self.set_grad()
@@ -58,6 +59,23 @@ def zero_padding(pad_size, img):
                 output[y][x] = img[y][x]
     return output
 
+def set_histogram(input, mag):
+    hist = np.zeros(shape = 10, dtype = float)
+    for ang in range(9):
+        idx = np.where(input < (ang + 1) * 20)
+        tmp2 = (input[idx] - ang * 20)/((ang + 1) * 20)
+        tmp1 = 1 -  tmp2
+        tmp2 *= mag[idx] 
+        tmp1 *= mag[idx]
+        hist[ang] += np.sum(tmp1)
+        hist[ang+1] += np.sum(tmp2)
+        input[idx] = 300
+        print(hist)
+        print("\n\n")
+    hist[0] += hist[9]
+    print(hist)
+
+    return hist[:9]
 
 
 
@@ -72,6 +90,9 @@ output = zero_padding(padding,data_x[0])
 
 grad = Gradient(input = output, pad = padding, stride = stride)
 grad_mag, grad_ang = grad.auto()
+grad_mag = grad_mag.reshape(-1)
+grad_ang = grad_ang.reshape(-1)
+hist = set_histogram(grad_ang,grad_mag)
 
 
 fig, ax = plt.subplots(2,1,figsize = (30,30))
@@ -80,8 +101,11 @@ ax[1].imshow(grad_ang,'gray')
 
 
         
-        
-        
+
+
+
+
+
 
 
 
